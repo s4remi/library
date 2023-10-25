3eedfcbd503a9f843f671367980180b300e28c82
s4remi <s4remi@gmail.com>
2023-10-25 16:46:09 -0700

    update the api

---
+++ db/My.Db.js
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+import { query } from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const connection_url = process.env.MONGO_URL;
+
+const MyDB = () => {
+  const myDb = {};
+
+  const connect = () => {
+    const client = new MongoClient(connection_url);
+    console.log(`thi is a connection url: ${connection_url}`);
+    const db = client.db("library");
+    return { client, db };
+  };
+
+  myDB.getBookByIsbn = async ({ query = {}, MaxElements = 2 }) => {
+    const { client, db } = connect();
+    const bookCollection = db.collection("books");
+    console.log(bookCollection);
+    try {
+      return await bookCollection.find(query).limit(MaxElements).toArray();
+    } finally {
+      console.log("db closing the connection");
+      client.close();
+    }
+  };
+  return myDB;
+};
+export const myDB = MyDB();
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,23 @@
-import express from "express";
-//the gos is to get a number as int or str from the html
-//side and use it to search for the match in db
+import { myDB } from "../db/My.Db";
 import bodyParser from "body-parser";
+
 export const router = express.Router();
 
-router.post("", (req, res) => {
+router.post("/searchByIsbn", bodyParser.json(), async (req, res) => {
   const isbn = req.body.isbn;
   const query = { ISBN: parseInt(isbn, 10) };
   console.log(query);
 
-  //const bookinfo= //
+  const bookInfo = await myDB.getBookByIsbn({ query });
+  console.log("inside the searchByIsbn");
+  console.log("the book info from getBookBYIsbn ", bookInfo);
+  if (bookInfo) {
+    return res
+      .status(200)
+      .json({ data: bookInfo, message: "Successfully found" });
+  } else {
+    return res.status(401).json({ message: "Didn't find anything" });
+  }
 });
+
+export default router;
