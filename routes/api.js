c1d80edf6f6b719df12ed868688c4d674ca18f71
s4remi <s4remi@gmail.com>
2023-10-25 13:57:12 -0700

    setting up the packages and add gitignote

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+import express from "express";
+//the gos is to get a number as int or str from the html
+//side and use it to search for the match in db
+import bodyParser from "body-parser";
+export const router = express.Router();
+
+router.post("", (req, res) => {
+  const isbn = req.body.isbn;
+  const query = { ISBN: parseInt(isbn, 10) };
+  console.log(query);
+
+  const bookinfo= //
+});
