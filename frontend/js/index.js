ef97ac9ad88d0f9f9cedc0b365150dfabfa98787
s4remi <s4remi@gmail.com>
2023-10-25 16:15:27 -0700

    set the router,add front end html file

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,20 @@
-console.log("hi there!");
+import apiRouter from "./routes/api.js";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.use(apiRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
